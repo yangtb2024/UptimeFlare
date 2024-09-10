ddd75e3ac5c9d63a9499f7d67a6a3f1152d5ec41
yangtb2024 <164613316+yangtb2024@users.noreply.github.com>
2024-09-10 13:22:31 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -35,7 +35,7 @@ const workerConfig = {
   ],
   notification: {
     appriseApiServer: "https://apprise.example.com/notify",
-    recipientUrl: "tgram://7353422993:AAFcVeqjM4SoA71lSuyk4RNs8mBHrHxTMis/7148175367",
+    recipientUrl: "tgram://7148175367/7353422993:AAFcVeqjM4SoA71lSuyk4RNs8mBHrHxTMis",
     timeZone: "Asia/Shanghai",
     gracePeriod: 5,
   },
